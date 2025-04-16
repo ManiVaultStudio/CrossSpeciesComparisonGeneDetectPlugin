1308a8cfe3532daba0739b546abe0e951a5ea6fa
Soumyadeep Basu <44787782+sbvis@users.noreply.github.com>
2025-04-16 17:38:00 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -58,12 +58,12 @@ class CrossSpeciesComparisonGeneDetectPluginConan(ConanFile):
         # Assign a version from the branch name
         branch_info = PluginBranchInfo(self.recipe_folder)
         self.version = branch_info.version
-        self.requires("CrossSpeciesComparisonTreeData/cytosploreviewer@lkeb/stable")
 
     def requirements(self):
         branch_info = PluginBranchInfo(self.__get_git_path())
         print(f"Core requirement {branch_info.core_requirement}")
         self.requires(branch_info.core_requirement)
+        self.requires("CrossSpeciesComparisonTreeData/cytosploreviewer@lkeb/stable")
 
     # Remove runtime and use always default (MD/MDd)
     def configure(self):
